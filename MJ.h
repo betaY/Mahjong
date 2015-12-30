bca35ed8203abd54b692d0be9eec6e9133b37ab4
beta <beta168921@gmail.com>
2015-12-30 12:56:54 +0800

    添加役种判断 method

--- MJ.h
+++ MJ.h
@@ -10,8 +10,8 @@ using namespace std;
 #define MJ_TIAO   3
 #define MJ_BING   4
 
-#define MJ_MO     TRUE
-#define MJ_DA     FALSE
+#define MJPAI_GETPAI     TRUE
+#define MJPAI_PUTPAI     FALSE
 
 struct stPAI {
   int m_type;       // 牌类型
@@ -69,7 +69,7 @@ public:
   //
   stGoodInfo *GetInfo();
   // 检查和牌
-  bool checkAllPai();
+  bool checkAllPai(bool GetOrPut);
   //
   void printAllPai();
   //
@@ -112,5 +112,119 @@ private:
   bool checkAAAABBBBCCCCPai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9, int iValue10, int iValue11, int iValue12);
   // 检测是否六连对
   bool checkAABBCCDDEEFFPai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9, int iValue10, int iValue11, int iValue12);
-  
-}
+
+  // 带雀头检测
+  // 检测是否胡牌（张）
+  bool check5Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5);
+  // 检测是否胡牌（张）
+  bool check8Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8);
+  // 检测是否胡牌（张）
+  bool check11Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9, int iValue10, int iValue11);
+  // 检测是否胡牌（张）
+  bool check14Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9, int iValue10, int iValue11, int iValue12, int iValue13, int iValue14);
+
+  // 不带雀头检测
+  // 检测是否胡牌（张）
+  bool check3Pai(int iValue1, int iValue2, int iValue3);
+  // 检测是否胡牌（张）
+  bool check6Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6);
+  // 检测是否胡牌（张）
+  bool check9Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9);
+  // 检测是否胡牌（张）
+  bool check12Pai(int iValue1, int iValue2, int iValue3, int iValue4, int iValue5, int iValue6, int iValue7, int iValue8, int iValue9, int iValue10, int iValue11, int iValue12);
+
+
+private:
+  // 役种判断
+
+  // 1.役牌
+  bool CheckYP_HU();
+  // 2.立直
+  bool CheckLZ_HU();
+  // 3.断幺
+  bool CheckDY_HU();
+  // 4.平和
+  bool CheckHU();
+  // 5.一发
+  bool Check1F_HU();
+  // 6.一杯口
+  bool Check1BK_HU();
+  // 7.抢杠
+  bool CheckQG_HU();
+  // 8.检测岭上开花
+  bool CheckLSKH_HU();
+  // 9.检测河底捞鱼
+  bool CheckHDLY_HU();
+  // 10.检测海底摸月
+  bool CheckHDMY_HU();
+  // 11.双立直
+  bool CheckSLZ_HU();
+  // 12.一气贯通
+  bool Check1QGT_HU();
+  // 13.小三元
+  bool CheckX3Y_HU();
+  // 14.三色同顺
+  bool Check3STS_HU();
+  // 15.三色同刻
+  bool Check3STK_HU();
+  // 16.三暗刻
+  bool Check3AK_HU();
+  // 17.三杠子
+  bool Check3GZ_HU();
+  // 18.全带幺九
+  bool CheckQDYJ_HU();
+  // 19.检测是否七对子
+  bool Check7D_HU();
+  // 20.对对和
+  bool CheckDDH_HU();
+  // 21.两杯口
+  bool CheckLBK_HU();
+  // 22.混一色
+  bool CheckH1S_HU();
+  // 23.混老头
+  bool CheckHLT_HU();
+  // 24.纯全带幺
+  bool CheckCQDY_HU();
+  // 25.流局满贯
+  bool CheckLJMG_HU();
+  // 26.清一色
+  bool CheckQ1S_HU();
+  // 27.小四喜
+  bool CheckX4X_HU();
+  // 28.天和
+  bool CheckTH_HU();
+  // 29.地和
+  bool CheckDH_HU();
+  // 30.人和
+  bool CheckRH_HU();
+  // 31.四杠子
+  bool Check4Gang_HU();
+  // 32.四暗刻
+  bool Check4AK_HU();
+  // 33.清老头
+  bool CheckQLT_HU();
+  // 34.绿一色
+  bool CheckL1S_HU();
+  // 35.九莲宝灯
+  bool Check9LBD_HU();
+  // 36.国士无双
+  bool CheckGSWS_HU();
+  // 37.大三元
+  bool CheckD3Y_HU();
+  // 38.大车轮
+  bool CheckDCL_HU();
+  // 39.大四喜
+  bool CheckD4X_HU();
+
+private:
+  // 听牌判断
+
+  // 听九莲宝灯
+  bool Check9LBD_TING();
+  // 听国士无双
+  bool CheckGSWS_TING();
+  // 听四暗刻
+  bool Check4AK_TING();
+  // 听牌
+  bool CheckTING();
+};
